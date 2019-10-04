d1ddb69ada50855129f4ea7e68e8695708b33b16
Peter Heldens <peter.heldens@live.nl>
2019-10-04 15:34:24 +0200

    Update main.blocks, custom.ts

--- custom.ts
+++ custom.ts
@@ -13,14 +13,14 @@ namespace MicrobitLive {
     /**
      * TODO: describe your function here
      * @param e describe parameter here, eg: "Microbit Live"
-     * @param s describe parameter here, eg: "Whats next ...."
+     * @param s describe parameter here, eg: "Whats"
      * @param t describe parameter here, eg: "twitter"
      * @param h describe parameter here, eg: "@peterheldens"
      * @param m describe parameter here, eg: "@4pip"
      */
 
     //% block
-    export function Event(e: string): void {
+    export function Seminar(e: string): void {
         // Add code here
     }
 
