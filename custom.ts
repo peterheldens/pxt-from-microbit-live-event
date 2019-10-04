aaa7d08ba0175a52b56ad346ff23076416660173
Peter Heldens <peter.heldens@live.nl>
2019-10-04 15:12:38 +0200

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -22,7 +22,7 @@ namespace MicrobitLive {
      * @param w describe parameter here, eg: "twitter"
      * @param s describe parameter here, eg: "@4pip"
      * @param t describe parameter here, eg: "@peterheldens"
-     * @param t describe parameter here, eg: "Whats NExt after 5 Lessons with the Micro:bit?"
+     * @param l describe parameter here, eg: "Whats NExt after 5 Lessons with the Micro:bit?"
      */
     //% block
     export function Pauline(w: string, s: string, e: MyEnum): void {
