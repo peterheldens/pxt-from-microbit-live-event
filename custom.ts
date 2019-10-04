a97615d786c9b7f82abb49a1a212960f9cb275bb
Peter Heldens <peter.heldens@live.nl>
2019-10-04 15:10:41 +0200

    Update main.blocks, custom.ts

--- custom.ts
+++ custom.ts
@@ -19,9 +19,10 @@ namespace MicrobitLive {
     /**
      * TODO: describe your function here
      * @param n describe parameter here, eg: 5
-     * @param t describe parameter here, eg: "twitter"
+     * @param w describe parameter here, eg: "twitter"
      * @param s describe parameter here, eg: "@4pip"
      * @param t describe parameter here, eg: "@peterheldens"
+     * @param t describe parameter here, eg: "Whats NExt after 5 Lessons with the Micro:bit?"
      */
     //% block
     export function Pauline(w: string, s: string, e: MyEnum): void {
@@ -38,5 +39,9 @@ namespace MicrobitLive {
         // Add code here
     }
 
+    //% block
+    export function SessionTitle(l: string): void {
+        // Add code here
+    }
     
 }
